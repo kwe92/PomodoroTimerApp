eac94a293e8332688a89f41966f7c79b17ccb4b5
Kwe <Kweayon@gmail.com>
2023-05-23 17:50:26 -0500

    Added theme variable

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import AppGlobalTheme from "./indexStyles";
+import AppTheme from "./styles/theme/AppTheme";
 
 const ele = document.getElementById("root");
 const root = ReactDOM.createRoot(ele as HTMLElement);
 
+const theme: ThemeInterface = AppTheme;
+
 root.render(
   <React.StrictMode>
-    <AppGlobalTheme />
+    <AppGlobalTheme theme={theme} />
     <App />
   </React.StrictMode>
 );
