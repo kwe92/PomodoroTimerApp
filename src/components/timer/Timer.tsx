270676bddc481dc3f7fca87572e078f9d8982b7c
Kwe <Kweayon@gmail.com>
2023-05-23 18:13:48 -0500

    Implemented options menu and highlight bubble JSX elements

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -1,10 +1,34 @@
 import React, { Fragment } from "react";
-import { OptionsMenu, TimerContentContainer, Title } from "./timerStyles";
+import {
+  OptionsMenu,
+  TimerContentContainer,
+  Title,
+  HighlightBubble,
+} from "./timerStyles";
+
+// State Machine to solve for ["pomodoro", "short break", "long break"]??
 const PomodoroTimer = () => {
+  const options: String[] = ["pomodoro", "short break", "long break"];
+  const optionListItems = options.map((option) => (
+    <li>
+      <HighlightBubble>
+        <p>{option}</p>
+      </HighlightBubble>
+    </li>
+  ));
+
   return (
     <TimerContentContainer>
       <Title>pomodoro</Title>
-      <OptionsMenu id="options-menu" />
+      {/* <OptionsMenu id="options-menu">{optionListItems}</OptionsMenu> */}
+
+      <OptionsMenu id="options-menu">
+        <HighlightBubble id="bubble">
+          <p>pomodoro</p>
+        </HighlightBubble>
+        <p>short break</p>
+        <p>long break</p>
+      </OptionsMenu>
     </TimerContentContainer>
   );
 };
