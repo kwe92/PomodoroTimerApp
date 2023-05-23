d7bccf592a3c41dbcbdb1a6249a9c26d53be0066
Kwe <Kweayon@gmail.com>
2023-05-23 16:59:30 -0500

    basic Row && Column flex boxes to extend from

---
+++ src/styles/theme/Flex.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Row = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  aign-items: center;
+`;
+
+export { Row, Column };
