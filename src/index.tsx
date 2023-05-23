f119d3d7473f4956e79d2e517a2997f91fecc3ad
Kwe <Kweayon@gmail.com>
2023-05-23 17:00:17 -0500

    Added App JSX Element

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AppGlobalTheme from "./indexStyles";
 
 const ele = document.getElementById("root");
 const root = ReactDOM.createRoot(ele as HTMLElement);
 
 root.render(
   <React.StrictMode>
+    <AppGlobalTheme />
     <App />
   </React.StrictMode>
 );
