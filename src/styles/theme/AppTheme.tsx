a2d957e507fbab70756dd9ad75adfb817f3c26c5
Kwe <Kweayon@gmail.com>
2023-05-23 17:15:20 -0500

    Theme Object

---
+++ src/styles/theme/AppTheme.tsx
@@ -0,0 +1,18 @@
+const AppTheme: ThemeInterface = {
+  darkColors: {
+    shade0: "rgba(22,25,50,1)",
+    shade1: "rgba(30,33,63,1)",
+  },
+  lightColors: {
+    shade0: "rgba(215,224,255,1)",
+    shade1: "rgba(239,241,250,1)",
+    shade2: "rgba(255,255,255,1)",
+  },
+  otherColors: {
+    purple0: "rgba(216,129,248,1)",
+    blue0: "rgba(112,243,248,1)",
+    red0: "rgba(248,112,112,1)",
+  },
+};
+
+export default AppTheme;
