175c3e24e27707fb9947a0870d3700d7ca45f806
Kwe <Kweayon@gmail.com>
2023-05-23 18:13:58 -0500

    Implemented options menu and highlight bubble JSX elements

--- src/components/timer/timerStyles.jsx
+++ src/components/timer/timerStyles.jsx
@@ -13,9 +13,21 @@ padding-bottom: 3.5rem;
 `
 
 const OptionsMenu = styled(Row)`
+justify-content: space-evenly;
 width: 23.3125rem;
 height: 3.9375rem;
 border-radius: 3.125rem;
+
+p{
+    font-size: 0.875rem;
+}
+`
+// TODO Needs work
+const HighlightBubble = styled(Row)`
+width:  7.5rem;
+height: 3rem;
+border-radius: 3.125rem;
 `
 
-export {TimerContentContainer,Title, OptionsMenu}
+
+export {TimerContentContainer,Title, OptionsMenu, HighlightBubble}
