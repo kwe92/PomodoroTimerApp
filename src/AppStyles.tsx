df7ce974c69d0bff7f830abc72be98298cce2705
Kwe <Kweayon@gmail.com>
2023-05-31 09:53:02 -0500

    AppStyles

---
+++ src/AppStyles.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Column } from "./styles/layout/Flex";
+
+const AppContentContainer = styled(Column)`
+  height: 100%;
+`;
+
+export { AppContentContainer };
