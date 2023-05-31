594a03bf37f4a740b56583d31213a934d2478434
Kwe <Kweayon@gmail.com>
2023-05-30 21:27:37 -0500

    refact: Decoupled OptMenu function from Timer.tsx

---
+++ src/components/options_menu/OptMenu.tsx
@@ -0,0 +1,29 @@
+import { HighlightBubble, OptionsMenu } from "../timer/timerStyles";
+
+export default function OptMenu(props: Options) {
+  return (
+    <OptionsMenu id="options-menu">
+      <HighlightBubble
+        id="bubble"
+        display={props.options.pomodoroOption}
+        onClick={props.options.handlePomodoroOption}
+      >
+        <p>pomodoro</p>
+      </HighlightBubble>
+
+      <HighlightBubble
+        id="bubble"
+        display={props.options.shortBreakOption}
+        onClick={props.options.handleShortBreakOption}
+      >
+        <p>short break</p>
+      </HighlightBubble>
+      <HighlightBubble
+        display={props.options.longBreakOption}
+        onClick={props.options.handleLongBreakOption}
+      >
+        <p>long break</p>
+      </HighlightBubble>
+    </OptionsMenu>
+  );
+}
