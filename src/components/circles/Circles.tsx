f767ed193f59b4481cb5aa95ec5e36ea6605beac
Kwe <Kweayon@gmail.com>
2023-05-30 22:21:06 -0500

    decoupled Circles

---
+++ src/components/circles/Circles.tsx
@@ -0,0 +1,40 @@
+import CircularProgress from "../circular_progress/CircularProgress";
+import {
+  Circle1,
+  Circle2,
+  Circle3,
+  CirclesContainer,
+  TimerText,
+} from "../timer/timerStyles";
+
+interface Props {
+  pctTimeRemaining: number;
+  styles: { p: Object; div: Object };
+  minutes: number;
+  seconds: number;
+  handleTimerText: Function;
+  timerText: string;
+}
+
+export default function Circles(props: Props) {
+  return (
+    <CirclesContainer>
+      <Circle1 id="c1" />
+      <Circle2 />
+      <Circle3>
+        <CircularProgress pctTimeRemaining={props.pctTimeRemaining} />
+        <p style={props.styles.p}>{`${props.minutes}:${
+          props.seconds < 10 ? `0${props.seconds}` : props.seconds
+        }`}</p>
+        <div style={props.styles.div} />
+        <TimerText
+          onClick={() => {
+            props.handleTimerText();
+          }}
+        >
+          {props.timerText}
+        </TimerText>
+      </Circle3>
+    </CirclesContainer>
+  );
+}
