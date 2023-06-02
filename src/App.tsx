3aed92f303ec5b11a7457687ebb3d58141d74fbd
Kwe <Kweayon@gmail.com>
2023-06-02 10:39:09 -0500

    refact: removed more state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { TimerSettings } from "react-timer-hook";
 import "react-circular-progressbar/dist/styles.css";
-import { useTimer } from "react-timer-hook";
 import { AppContentContainer } from "./AppStyles";
 import AppTitle from "./components/title/Title";
 import OptMenu from "./components/options_menu/OptMenu";
@@ -10,87 +8,42 @@ import SettingsIcon from "./components/settings_icon/SettingsIcon";
 import SettingsMenu from "./components/settings/SettingsMenu";
 import { observer } from "mobx-react";
 import useStores from "./stores/Stores";
+import OptMenuHandles from "./components/options_menu/OptMenuHandles";
 
 export default observer(function App() {
-  const { optionsStore, timeStore } = useStores();
-  const [timer, setTimer] = useState(new Date());
+  const { optionsStore, timeStore, dateModel, timerModel, totalTimeModel } =
+    useStores();
+  const { seconds, minutes, pause, resume, restart } = timerModel.useTimer(
+    dateModel.timer
+  );
   const [timerText, setTimerText] = useState("START");
-  const [totalTime, setTotalTime] = useState(0);
-
-  const timerSettings: TimerSettings = {
-    autoStart: false,
-    expiryTimestamp: timer,
-    onExpire: () => console.warn("onExpire called"),
-  };
-  const {
-    seconds,
-    minutes,
-    hours,
-    days,
-    isRunning,
-    start,
-    pause,
-    resume,
-    restart,
-  } = useTimer(timerSettings);
-
   const [isOpened, setIsOpened] = useState(false);
 
+  const optMenuHandles = new OptMenuHandles({
+    timeStore,
+    optionsStore,
+    dateModel,
+    pause,
+    restart,
+    setTimerText,
+    totalTimeModel,
+  });
   useEffect(() => {
-    setTimer(new Date());
-    timer.setSeconds(timer.getSeconds() + (60 * timeStore.times.pomodoro) / 2);
-    setTotalTime(60 * timeStore.times.pomodoro);
-    restart(timer, false);
+    dateModel.setTimer(new Date());
+    dateModel.timer.setSeconds(
+      dateModel.timer.getSeconds() + (60 * timeStore.times.pomodoro) / 2
+    );
+    totalTimeModel.setTotalTime(60 * timeStore.times.pomodoro);
+    restart(dateModel.timer, false);
   }, [timeStore.times.pomodoro]);
 
   let timeRemaining = minutes * 60 + seconds;
-  let pctTimeRemaining = (timeRemaining / totalTime) * 100;
+  let pctTimeRemaining = (timeRemaining / totalTimeModel.totalTime) * 100;
 
   // TODO: expand on this if statement to sound an alarm when the timer reaches 0
   if (timeRemaining == 0) {
     console.log("TIME ELAPSED!!!");
   }
-
-  const handleSwitchOption = (time: number) => {
-    pause();
-    setTimer(new Date());
-    timer.setSeconds(timer.getSeconds() + 60 * time);
-    restart(timer, false);
-    setTimerText("START");
-  };
-
-  const handlePomodoroOption = () => {
-    handleSwitchOption(timeStore.times.pomodoro);
-    setTotalTime(60 * timeStore.times.pomodoro);
-
-    if (optionsStore.options.pomodoroOption) {
-      return;
-    } else {
-      optionsStore.options.setPomodoroOption();
-    }
-  };
-
-  const handleShortBreakOption = () => {
-    handleSwitchOption(timeStore.times.shortBreak);
-    setTotalTime(60 * timeStore.times.shortBreak);
-
-    if (optionsStore.options.shortBreakOption) {
-      return;
-    } else {
-      optionsStore.options.setShortBreakOption();
-    }
-  };
-
-  const handleLongBreakOption = () => {
-    handleSwitchOption(timeStore.times.longBreak);
-    setTotalTime(60 * timeStore.times.longBreak);
-
-    if (optionsStore.options.longBreakOption) {
-      return;
-    } else {
-      optionsStore.options.setLongBreakOption();
-    }
-  };
 
   const handleTimerText = () => {
     if (timerText == "START") {
@@ -114,11 +67,11 @@ export default observer(function App() {
 
   const options = {
     pomodoroOption: optionsStore.options.pomodoroOption,
-    handlePomodoroOption: handlePomodoroOption,
+    handlePomodoroOption: optMenuHandles.handlePomodoroOption,
     shortBreakOption: optionsStore.options.shortBreakOption,
-    handleShortBreakOption: handleShortBreakOption,
+    handleShortBreakOption: optMenuHandles.handleShortBreakOption,
     longBreakOption: optionsStore.options.longBreakOption,
-    handleLongBreakOption: handleLongBreakOption,
+    handleLongBreakOption: optMenuHandles.handleLongBreakOption,
   };
 
   return (
