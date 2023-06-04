f276928792b2596e4261b9a76c30cd751d215bc8
Kwe <Kweayon@gmail.com>
2023-06-04 18:10:33 -0500

    Removed global theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import AppGlobalTheme from "./indexStyles";
-import AppTheme from "./styles/theme/AppTheme";
 
 const ele = document.getElementById("root");
 const root = ReactDOM.createRoot(ele as HTMLElement);
 
-const theme: ThemeInterface = AppTheme;
-
 root.render(
   <React.StrictMode>
-    <AppGlobalTheme theme={theme} />
     <App />
   </React.StrictMode>
 );
