94114baf38ba9d4bf94dbf13f7e5ca797425c190
Kwe <Kweayon@gmail.com>
2023-06-05 12:28:53 -0500

    DisplayTimerOptionsModel

---
+++ src/stores/display_timer/DisplayTimerModel.tsx
@@ -0,0 +1,21 @@
+import { types } from "mobx-state-tree";
+
+const DisplayTimerOptionsModel = types
+  .model({
+    pomodoroTimerOptions: types.boolean,
+    shortBreakTimerOptions: types.boolean,
+    longBreakTimerOptions: types.boolean,
+  })
+  .actions((self) => ({
+    displayPomodoro() {
+      self.pomodoroTimerOptions = !self.pomodoroTimerOptions;
+    },
+    displayShortBreak() {
+      self.shortBreakTimerOptions = !self.shortBreakTimerOptions;
+    },
+    displayLongBreak() {
+      self.longBreakTimerOptions = !self.longBreakTimerOptions;
+    },
+  }));
+
+export default DisplayTimerOptionsModel;
