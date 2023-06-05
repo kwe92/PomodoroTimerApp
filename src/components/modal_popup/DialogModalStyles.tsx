5a094297ff9064c456d65fb2699ffe48d1e30c1e
Kwe <Kweayon@gmail.com>
2023-06-05 16:54:04 -0500

    Update

--- src/components/modal_popup/DialogModalStyles.tsx
+++ src/components/modal_popup/DialogModalStyles.tsx
@@ -3,15 +3,15 @@ import styled from "styled-components";
 const Container = styled.dialog`
   width: 100vw;
   height: 100vh;
-  border-radius: 8px;
-  border: 1px solid #888;
+  // border-radius: 8px;
+  border: none;
   z-index: 1;
   background: rgba(22, 25, 50, 0.725);
   // TODO: remove color property
   color: white;
 
   ::backdrop {
-    background: rgba(0, 0, 0, 0.3);
+    background: rgba(0, 0, 0, 0);
   }
 `;
 
